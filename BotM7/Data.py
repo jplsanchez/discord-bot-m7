c75aa446fee6e25b1804c966e94a8917ae94b647
jplsanchez <jpaulo_sanchez@hotmail.com>
2022-01-12 12:29:20 -0300

    fix: SonarCloud Errors

--- BotM7/Data.py
+++ BotM7/Data.py
@@ -8,7 +8,7 @@ class Data:
     DB_USER = os.environ.get("DB_USER")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
-    def execute_query(query):
+    def __execute_query(query):
         try:
             con = connection.MySQLConnection(
                 user=Data.DB_USER,
@@ -31,25 +31,25 @@ class Data:
             return result
 
         except:
-            print("ERROR: Erro de conexão tag=execute_query")
+            print("ERROR: Erro de conexão tag=__execute_query")
 
-    def add_new_participant(author_id, author):
+    def add_new_participant(self, author_id, author):
         add_person_query = (
             "INSERT INTO Ranking "
             + "(ID, Points, PersonName, IsActive) "
             + f'VALUES ("{str(author_id)}", 0, "{author}", 0)'
         )
 
-        Data.execute_query(add_person_query)
+        Data.__execute_query(add_person_query)
 
-    def get_general_ranking():
+    def get_general_ranking(self):
         get_ranking_query = (
             "SELECT PersonName, Points FROM Ranking ORDER BY Points DESC"
         )
 
-        return Data.execute_query(get_ranking_query)
+        return Data.__execute_query(get_ranking_query)
 
-    def get_general_overall_ranking():
+    def get_general_overall_ranking(self):
         get_overall_ranking_query = (
             "SELECT r.PersonName, (r.Points + lr.Points) as Pts "
             + "FROM Ranking AS r "
@@ -58,21 +58,21 @@ class Data:
             + "ORDER BY Pts DESC; "
         )
 
-        return Data.execute_query(get_overall_ranking_query)
+        return Data.__execute_query(get_overall_ranking_query)
 
-    def get_points_by_id(id):
+    def get_points_by_id(self, id):
         get_player_ranking_query = f"SELECT Points FROM Ranking WHERE ID = {str(id)}"
 
-        return Data.execute_query(get_player_ranking_query)[0][0]
+        return Data.__execute_query(get_player_ranking_query)[0][0]
 
-    def update_points(id, points):
+    def update_points(self, id, points):
         update_points_query = (
             f'UPDATE Ranking SET Points = {str(points)} WHERE ID = "{str(id)}"'
         )
 
-        Data.execute_query(update_points_query)
+        Data.__execute_query(update_points_query)
 
-    def add_new_image(author_id, url, filename):
+    def add_new_image(self, author_id, url, filename):
         now_string = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         add_image_query = (
@@ -80,9 +80,9 @@ class Data:
             + f'VALUES ("{author_id}", "{now_string}", "{url}", {1}, "{filename}")'
         )
 
-        Data.execute_query(add_image_query)
+        Data.__execute_query(add_image_query)
 
-    def has_recent_image(author_id):
+    def has_recent_image(self, author_id):
         twenty_minutes = timedelta(minutes=20)
         now = datetime.now()
         recent_time = now - twenty_minutes
@@ -92,13 +92,13 @@ class Data:
             "SELECT PersonID FROM ImageRef "
             + f'WHERE PersonID = "{author_id}" AND DateAdded > "{recent_time_string}" '
         )
-        result = Data.execute_query(get_recent_images_query)
+        result = Data.__execute_query(get_recent_images_query)
         if result == []:
             return False
         return True
 
-    def set_points(id: str, points: float):
+    def set_points(self, id: str, points: float):
         set_points_query = (
             f'UPDATE ranking SET points = {str(points)} where ID = "{id}"'
         )
-        return Data.execute_query(set_points_query)
+        return Data.__execute_query(set_points_query)
